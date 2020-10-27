162d33b1bdea553f35c63e73eaef2ac9acd97f8b
thomvanbesouw <68852282+thomvanbesouw@users.noreply.github.com>
2020-10-27 22:51:36 +0100

    Add files via upload

---
+++ Morse naar Nederlands en terug/Morse naar Nederlands en terug/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Morse_naar_Nederlands_en_terug
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.MorseKnop = new System.Windows.Forms.Button();
+            this.NederlandsKnop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(498, 228);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // MorseKnop
+            // 
+            this.MorseKnop.Location = new System.Drawing.Point(311, 266);
+            this.MorseKnop.Name = "MorseKnop";
+            this.MorseKnop.Size = new System.Drawing.Size(195, 66);
+            this.MorseKnop.TabIndex = 1;
+            this.MorseKnop.Text = "Vertaal naar Morse";
+            this.MorseKnop.UseVisualStyleBackColor = true;
+            this.MorseKnop.Click += new System.EventHandler(this.MorseKnop_Click);
+            // 
+            // NederlandsKnop
+            // 
+            this.NederlandsKnop.Location = new System.Drawing.Point(311, 338);
+            this.NederlandsKnop.Name = "NederlandsKnop";
+            this.NederlandsKnop.Size = new System.Drawing.Size(195, 70);
+            this.NederlandsKnop.TabIndex = 2;
+            this.NederlandsKnop.Text = "Vertaal terug";
+            this.NederlandsKnop.UseVisualStyleBackColor = true;
+            this.NederlandsKnop.Click += new System.EventHandler(this.NederlandsKnop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NederlandsKnop);
+            this.Controls.Add(this.MorseKnop);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button MorseKnop;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button NederlandsKnop;
+    }
+}
+
---
+++ Morse naar Nederlands en terug/Morse naar Nederlands en terug/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Morse_naar_Nederlands_en_terug
+{
+    public partial class Form1 : Form
+    {
+        string OriginalText = "";
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MorseKnop_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+            string Morse = "";
+            OriginalText = textBox1.Text;
+
+            
+            foreach (char c in textBox1.Text.ToLower())
+            {
+               
+
+                switch (c)
+                    {
+                        case 'a':
+                            Morse += ". - / ";
+                            break;
+                        case 'b':
+                            Morse += "- . . . / ";
+                            break;
+                        case 'c':
+                            Morse += "- . - . / ";
+                            break;
+                        case 'd':
+                            Morse += ". - - / ";
+                            break;
+                        case 'e':
+                            Morse += ". / ";
+                            break;
+                        case 'f':
+                            Morse += ". . - . / ";
+                            break;
+                        case 'g':
+                            Morse += "- - . / ";
+                            break;
+                        case 'h':
+                            Morse += ". . . . / ";
+                            break;
+                        case 'i':
+                            Morse += ". . / ";
+                            break;
+                        case 'j':
+                            Morse += ". - - - / ";
+                            break;
+                        case 'k':
+                            Morse += "- . - / ";
+                            break;
+                        case 'l':
+                            Morse += ". - . . / ";
+                            break;
+                        case 'm':
+                            Morse += "- - / ";
+                            break;
+                        case 'n':
+                            Morse += "- . / ";
+                            break;
+                        case 'o':
+                            Morse += "- - - / ";
+                            break;
+                        case 'p':
+                            Morse += ". - - . / ";
+                            break;
+                        case 'q':
+                            Morse += "- - . - / ";
+                            break;
+                        case 'r':
+                            Morse += ". - . / ";
+                            break;
+                        case 's':
+                            Morse += ". . . / ";
+                            break;
+                        case 't':
+                            Morse += "- / ";
+                            break;
+                        case 'u':
+                            Morse += ". . - / ";
+                            break;
+                        case 'v':
+                            Morse += ". . . - / ";
+                            break;
+                        case 'w':
+                            Morse += ". - - / ";
+                            break;
+                        case 'x':
+                            Morse += "- . . - / ";
+                            break;
+                        case 'y':
+                            Morse += "- . - - / ";
+                            break;
+                        case 'z':
+                            Morse += "- - . . / ";
+                            break;
+                        case ' ':
+                            Morse += "   ";
+                            break;
+                    default:
+                        Morse += c + " ";
+                        break;
+                    
+
+
+                }
+                switch (a)
+                {
+                    case 0:
+                        if (textBox1.Text.Length > 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 1:
+                        if (textBox1.Text.Length > b + 72 )
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 2:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 3:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 4:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 5:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 6:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 7:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 8:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 9:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 10:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 11:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 12:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 13:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 14:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 15:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 16:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+                    case 17:
+                        if (textBox1.Text.Length > b + 72)
+                        {
+                            b = textBox1.Text.Length;
+                            textBox1.Text += "\n";
+                            a++;
+                        }
+                        break;
+
+                }
+                
+            }
+            textBox1.Clear();
+            textBox1.Text += Morse;
+        }
+
+        private void NederlandsKnop_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Text += OriginalText;
+        }
+    }
+}
